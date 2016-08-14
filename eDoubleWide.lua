5c7a7a6a3dd8a28f918ade9ff947c1b056539fd1
Ession <mail@mathiasjost.com>
2016-08-14 03:46:06 +0200

    * Bumped Version for Legion
    Signed-off-by: Ession <mail@mathiasjost.com>

--- eDoubleWide.lua
+++ eDoubleWide.lua
@@ -4,7 +4,7 @@
 --
 --    Description:  Doubles the size of the professions windows.
 --
---        Version:  6.2.1
+--        Version:  7.0.1
 --
 --         Author:  Mathias Jost (mail@mathiasjost.com)
 --
